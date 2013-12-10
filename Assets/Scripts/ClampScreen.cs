6dcf460fec4e603257de9f9a8763329f76a12f1a
Amit Bader <capt.dreamer@gmail.com>
2013-12-10 12:07:12 -0600

    finish movement system (with basic clamping)

---
+++ Assets/Scripts/ClampScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClampScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ClampCheck (this.transform.position);
+	}
+
+	void ClampCheck(Vector3 loc)
+	{
+		Vector3 position = loc;
+		if (position.y > 3 || position.y < -3)
+		{
+			position.y *= -1;
+			Debug.Log (position);
+			this.transform.position = position;
+		}
+		if (position.x > 4 || position.x < -4)
+		{
+			position.x *= -1;
+			Debug.Log (position);
+			this.transform.position = position;
+		}
+	}
+}
